309882fa6ef078f254da69b87c3589ee9cf07cae
AdrianoBendazzoli <adrianobendazzoli911@gmail.com>
2026-04-16 10:18:21 -0300

    .

--- aula06/api.php
+++ aula06/api.php
@@ -7,21 +7,43 @@ $metodo= $_SERVER ['REQUEST_METHOD'];
 // echo "Método da requisição: ". $metodo;
 
 //CONTEÚDO
-// $usuarios = [
-//     ["id" => 1, "nome" => "Adriano", "email" => "Adriano@email.com"], 
-//     ["id" => 2, "nome" => "Beatriz", "email" => "Beatriz@email.com"]
-// ];
+ //$usuarios = [
+    // ["id" => 1, "nome" => "Adriano", "email" => "Adriano@email.com"], 
+    // ["id" => 2, "nome" => "Beatriz", "email" => "Beatriz@email.com"]
+ //];
 
 // Converte para JSON e retorna
-// echo json_encode($usuarios);
+ //echo json_encode($usuarios);
+    $arquivo = 'usuarios.json';
+
+    //verifica se o arquivo existe, sxe não ele cria um com um array vazio
+    if (file_exists($arquivo)) {
+        file_put_contents($arquivo, json_encode([], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+    }
+
+    //lê o conteudo json
+    $usuarios = json_decode(file_get_contents($arquivo),true);
 
     switch ($metodo) {
         case 'GET':
-            echo " ações do Método Get";
+            //echo " ações do Método Get";
+            echo json_encode($usuarios);
         break;
 
         case 'POST':
-            echo " ações do Método POST";
+            //echo " ações do Método POST";
+            $dados = json_decode(file_get_contents('php://input'),true);
+            //print_r($dados);
+            $NovoUser = [
+                "id"=> $dados["id"],
+                "nome"=> $dados["nome"],
+                "email"=> $dados["email"]
+            ];
+
+            //adiciona o novo usuário ao array existente
+            array_push($usuarios, $NovoUser);
+            echo json_encode('Usuário inserido com sucesso!!');
+            print_r($usuarios);
         break;
         
         default:
